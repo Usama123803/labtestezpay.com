11e161c27a062f123ef2425d7104b0f8101790e9
Sajid Ijaz <sajid@smarttechnologist.com>
2020-08-11 21:16:14 +0500

    Location modification

--- app/Admin/Controllers/LocationController.php
+++ app/Admin/Controllers/LocationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Helper\AdminHelper;
 use App\Location;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
@@ -29,9 +30,12 @@ class LocationController extends AdminController
         $grid->column('id', __('Id'));
         $grid->column('name', __('Name'));
         $grid->column('status', __('Status'));
-        $grid->column('deleted_at', __('Deleted at'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+//        $grid->column('deleted_at', __('Deleted at'));
+//        $grid->column('created_at', __('Created at'));
+//        $grid->column('updated_at', __('Updated at'));
+        AdminHelper::gridDateFormat($grid, 'deleted_at', 'Deleted at');
+        AdminHelper::gridDateFormat($grid, 'created_at', 'Created at');
+        AdminHelper::gridDateFormat($grid, 'updated_at', 'Updated at');
 
         return $grid;
     }
@@ -49,9 +53,13 @@ class LocationController extends AdminController
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
         $show->field('status', __('Status'));
-        $show->field('deleted_at', __('Deleted at'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
+//        $show->field('deleted_at', __('Deleted at'));
+//        $show->field('created_at', __('Created at'));
+//        $show->field('updated_at', __('Updated at'));
+
+        AdminHelper::gridDateFormat($show, 'deleted_at', 'Deleted at');
+        AdminHelper::gridDateFormat($show, 'created_at', 'Created at');
+        AdminHelper::gridDateFormat($show, 'updated_at', 'Updated at');
 
         return $show;
     }
