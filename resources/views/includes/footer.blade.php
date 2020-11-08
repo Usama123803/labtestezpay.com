7f26164692c15e08fbf928358160de391ea63aa5
Naib Ali <naib@smarttechnologist.com>
2020-11-08 17:20:26 +0500

    add link to the footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,11 +3,11 @@
         <div class="row align-items-center">
             <div class="col-lg-4 text-lg-left">Copyright © labtestezpay</div>
             
-            <!--<div class="col-lg-4 my-3 my-lg-0">
-                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+            <div class="col-lg-4 my-3 my-lg-0">
+            <!--    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                 <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-            </div> -->
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>-->
+            </div> 
 
             <div class="col-lg-4 text-lg-right">
                 <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/Labtestezpay"><i class="fab fa-facebook-f"></i></a>
