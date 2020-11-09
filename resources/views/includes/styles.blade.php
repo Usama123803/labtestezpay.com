19651853d9db74e4dc57dd7dc4300f6d94dad9ee
Naib Ali <naib@smarttechnologist.com>
2020-11-09 16:01:18 +0500

    change in style php for datetimepicker

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -3,3 +3,8 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+
+<link href="{!! asset('assets/css/bootstrap.min.css') !!}" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link href="{!! asset('assets/css/bootstrap-datetimepicker.min.css') !!}" rel="stylesheet">
