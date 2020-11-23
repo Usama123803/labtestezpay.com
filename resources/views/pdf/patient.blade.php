98c818a48e5ca48a786c0677a4b0691398b2cb64
Naib Ali <naib@smarttechnologist.com>
2020-11-23 17:26:19 +0500

    change logo extension

--- resources/views/pdf/patient.blade.php
+++ resources/views/pdf/patient.blade.php
@@ -61,7 +61,7 @@
     <tr>
         <td>
             <div style="display: inline-block;float: left;">
-                                <img src="{{ public_path('assets/images/labtest-logo.png') }}" alt="LabTest-Logo" width="150" />
+                                <img src="{{ public_path('assets/images/labtest-logo.jpg') }}" alt="LabTest-Logo" width="150" />
 {{--                                <img src="http://labwork360.com/assets/images/labtest-logo.PNG" alt="LabTest-Logo" width="150" />--}}
             </div>
            <!-- <div style="display: inline-block;float: right">
