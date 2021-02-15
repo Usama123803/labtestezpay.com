306792f45294db3c113f9f636530f1f6168903b0
Naib Ali <naib@smarttechnologist.com>
2021-02-15 06:26:01 +0000

    Update terms-and-condition.blade.php

--- resources/views/pages/terms-and-condition.blade.php
+++ resources/views/pages/terms-and-condition.blade.php
@@ -7,7 +7,16 @@
 {{--  ADD HTML HERE  --}}
 <section>
  <p>
- Labtest diagnostics & Labtest EZPay try their best to deliver the results on time but does not guarantee the delivery of results in time and accept no responsibility for it.
+ 1.  Labtest diagnostics & Labtest EZPay try their best to deliver the results on time but do not guarantee the delivery of results in time and accept no responsibility for it.
+ </p>
+ <p>
+2. We may use the information for our person and promotional services which you provide by filling in forms on the Website, such as Your personal details (e.g., Your name, date of birth, etc.) or contact details (e.g., telephone number, email address, postal address, etc.). This includes information provided at the time of registering to use the registration-only sections of the Website.
+</p>
+<p>
+3. We may share your results with your healthcare provider or the clinic we are using as our satellite location.
+</p>
+<p>
+4. By agreeing to our terms and conditions the guardians of under age children are consenting our staff to collect the specimen (blood or nasal as the case may be) needed for the particular tests.
 </p>
 </section>
 @endsection
